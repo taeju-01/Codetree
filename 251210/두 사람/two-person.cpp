ec5544b3191487d998d38919b1780e7a35c9160f
taeju-01 <taeju1476@gmail.com>
2025-12-10 21:36:02 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251210/두 사람/two-person.cpp
+++ 251210/두 사람/two-person.cpp
@@ -6,9 +6,9 @@ int main() {
     int a_age,b_age;
     char a,b;
     cin>>a_age>>a>>b_age>>b;
-    if((a_age>=19||a=='M'))
+    if((a_age>=19&&a=='M'))
     cout<<1;
-    else if((b_age>=19||b=='M')){
+    else if((b_age>=19&&b=='M')){
             cout<<1;
         }
         else
