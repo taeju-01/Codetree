8a2f69ad6e4416f0bb43ea14a2545739f91ca85f
taeju-01 <taeju1476@gmail.com>
2025-12-10 20:01:53 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251210/삼항연산자/ternary-operator.cpp
+++ 251210/삼항연산자/ternary-operator.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     // Please write your code here.
     int score;
     cin>>score;
-    int result=score==0?"pass":"failure";
+    string result=score==100 ? "pass":"failure";
     cout<<result;
     return 0;
 }
