21d43b5fbe51da5ebd9f543aacb1f942a21d1850
taeju-01 <taeju1476@gmail.com>
2025-12-10 17:52:37 +0900

    Title: 체질량지수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251210/체질량지수/bmi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int h,w;
+    cin>>h>>w;
+    int b=(10000*w)/(h*h);
+    if(b>=25){
+        cout<<b<<endl<<Obesity;
+    }
+    return 0;
+}
