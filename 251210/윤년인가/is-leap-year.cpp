c8fc24822438a00a0df5108045c92546234f22da
taeju-01 <taeju1476@gmail.com>
2025-12-10 21:54:02 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251210/윤년인가/is-leap-year.cpp
+++ 251210/윤년인가/is-leap-year.cpp
@@ -6,10 +6,17 @@ int main() {
     int a;
     cin>>a;
     if(a%4==0){
-        if(a%400==0)
-        cout<<"true";
+        if(a%100==0){
+            if(a%400==0){
+            cout<<"true";
+            }
+            else{
+                cout<<"false";
+            }
+        }
+       
         else
-        cout<<"false";
+        cout<<"true";
     }
     else
     cout<<"false";
