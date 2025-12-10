d0a1b5df5087f38620bbef2db9c7448b461b6ee2
taeju-01 <taeju1476@gmail.com>
2025-12-10 20:42:17 +0900

    Title: 범위 안의 수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251210/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    if(a>=20&&a<=20)
+    cout<<"yes";
+    else
+    cout<<"no";
+    return 0;
+}
