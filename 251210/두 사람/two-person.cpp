0466bd1d9acd2b4a01c66f31ecd757cac2362892
taeju-01 <taeju1476@gmail.com>
2025-12-10 21:32:53 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251210/두 사람/two-person.cpp
+++ 251210/두 사람/two-person.cpp
@@ -6,8 +6,11 @@ int main() {
     int a_age,b_age;
     char a,b;
     cin>>a_age>>a>>b_age>>b;
-    if((a_age>=19||b_age>=19)&&(a=='M'||b=='M'))
-    cout<<1;
+    if((a_age>=19||b_age>=19)){
+        if((a=='M'||b=='M')){
+            cout<<1;
+        }
+    
     else
     cout<<0;
     return 0;
