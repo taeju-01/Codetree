cd3d420e83d573101f6ea3c070fb0f80f5d92562
taeju-01 <taeju1476@gmail.com>
2025-12-10 21:29:47 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251210/두 사람/two-person.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a_age,b_age;
+    char a,b;
+    cin>>a_age>>a>>b_age>>b;
+    if((a_age>=19||b_age>=19)&&(a=='M'||b=='M'))
+    cout<<1;
+    else
+    cout<<0;
+    return 0;
+}
