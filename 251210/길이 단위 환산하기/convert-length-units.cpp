9a0ab969251e1137538d17742458b72177e78fe0
taeju-01 <taeju1476@gmail.com>
2025-12-10 16:27:20 +0900

    Title: 길이 단위 환산하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double ft=30.48;
+    double a;
+    cin>>a;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<a*ft;
+    return 0;
+}
