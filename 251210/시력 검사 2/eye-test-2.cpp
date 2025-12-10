0da4e11c284e8b1e78e143e7022b433cc764d0ac
taeju-01 <taeju1476@gmail.com>
2025-12-10 20:07:24 +0900

    Title: 시력 검사 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    cin>>a;
+    if(a>=1.0){
+        cout<<"High";
+    }
+    else if(a>=0.5){
+        cout<<"Middle";
+    }
+    else
+    cout<<"Low";
+    return 0;
+}
