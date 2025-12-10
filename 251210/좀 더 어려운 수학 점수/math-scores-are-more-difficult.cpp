6c18fda26fd1888ca11ebdca7ff94814a9c6f27a
taeju-01 <taeju1476@gmail.com>
2025-12-10 21:24:57 +0900

    Title: 좀 더 어려운 수학 점수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251210/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int mat_a,eng_a,mat_b,eng_b;
+    cin>>mat_a>>eng_a>>mat_b>>eng_b;
+    if(mat_a>mat_b)
+    cout<<"A";
+    else
+    cout<<"B";
+    if(mat_a==mat_b){
+        if(eng_a>eng_b){
+            cout<<"A";
+        }
+        else
+        cout<<"B";
+    }
+
+    return 0;
+}
