4f899b497e16cefa3bf78459638a946f79628648
taeju-01 <taeju1476@gmail.com>
2025-12-10 20:30:13 +0900

    Title: 4가지 관계연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    cout<<(a>=b)<<endl;
+    cout<<(a>b)<<endl;
+    cout<<(b>=a)<<endl;
+    cout<<(b>a)<<endl;
+    return 0;
+}
