564235bfb2e870c0cfc8c191ab367cc5279ff1da
taeju-01 <taeju1476@gmail.com>
2025-12-10 20:08:45 +0900

    Title: 살 수 있는 물건, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    if(n>=3000){
+        cout<<"book";
+    }
+    else if(n>=1000){
+        cout<<"mask";
+    }
+    else
+    cout<<"no";
+    return 0;
+}
