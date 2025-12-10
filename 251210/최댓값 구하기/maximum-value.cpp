bd8536d015a5f28d3c4978fac779592ef9f2da64
taeju-01 <taeju1476@gmail.com>
2025-12-10 22:02:27 +0900

    Title: 최댓값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251210/최댓값 구하기/maximum-value.cpp
+++ 251210/최댓값 구하기/maximum-value.cpp
@@ -5,9 +5,9 @@ int main() {
     // Please write your code here.
     int a,b,c;
     cin>>a>>b>>c;
-    if(a>b&&a>c)
+    if(a>=b&&a>=c)
     cout<<a;
-    else if(b>c&&b>a)
+    else if(b>=c&&b>=a)
     cout<<b;
     else
     cout<<c;
