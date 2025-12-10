375b5012d06766ddee095b52f1f01c854ea08d4a
taeju-01 <taeju1476@gmail.com>
2025-12-10 17:55:02 +0900

    Title: 정사각형의 넓이, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    if(n<5){
+        cout<<n*n<<endl<<"tiny";
+    }
+    else
+    cout<<n*n;
+    return 0;
+}
