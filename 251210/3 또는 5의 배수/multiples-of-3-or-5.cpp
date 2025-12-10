d1b488ae38e74703c00c7f7fef1b000813dc0617
taeju-01 <taeju1476@gmail.com>
2025-12-10 20:26:20 +0900

    Title: 3 또는 5의 배수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    if(a%3==0)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+    if(a%5==0)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO";
+    return 0;
+}
