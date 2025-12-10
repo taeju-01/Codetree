959ba4c82129452a262a58e99a51fd265911d759
taeju-01 <taeju1476@gmail.com>
2025-12-10 17:37:38 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin>>a>>b>>c;
+    int d=a+b+c;
+    int e=d/3;
+    cout<<d<<endl<<e<<endl<<d-e;
+    return 0;
+}
