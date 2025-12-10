44ad42b554fed664b4a3a76f389a2a78bcd5b6c5
taeju-01 <taeju1476@gmail.com>
2025-12-10 16:29:20 +0900

    Title: 실수 입력받아 계산 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    cin>>a;
+    cout<<fixed;
+    cout.precision(2);
+    cout<<a+1.5;
+    return 0;
+}
