049f10b11d32bd22dc4e7fee988293bbc2cc3586
taeju-01 <taeju1476@gmail.com>
2025-12-11 16:04:53 +0900

    Title: 별 N개 출력하기, Time: 1086ms, Memory: 2MB, Status: Time Limit Exceed - Codetree

---
+++ 251211/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        cout<<"*"<<endl;
+    }
+    return 0;
+}
