3e3285f1a3715b625c3749c6c94e5b340657f3e4
taeju-01 <taeju1476@gmail.com>
2025-12-11 20:44:23 +0900

    Title: 등장횟수 확인, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int cnt=0;
+    int n;
+    for(int i=0;i<5;i++){
+        cin>>n;
+        if(n%2==0){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
