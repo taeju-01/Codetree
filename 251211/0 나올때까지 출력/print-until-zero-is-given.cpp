750f6461af93b45f6e99320cbe230cacf786cf7f
taeju-01 <taeju1476@gmail.com>
2025-12-11 22:52:10 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    while(1){
+        int n;
+        cin>>n;
+        if(n==0){
+            break;
+        }
+        cout<<n<<endl;
+    }
+    return 0;
+}
