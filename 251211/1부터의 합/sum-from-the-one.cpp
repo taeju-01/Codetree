a643a710ce49ace83c8bf69ed513dd19abd3e17f
taeju-01 <taeju1476@gmail.com>
2025-12-11 21:59:58 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=100;i++){
+        sum+=i;
+        if(sum>=n){
+            cout<<i;
+            break;
+        }
+    }
+    // cout<<i;
+    return 0;
+}
