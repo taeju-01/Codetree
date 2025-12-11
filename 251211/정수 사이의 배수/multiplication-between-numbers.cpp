99007439e325dcbf9225da1c108eafddeea514cc
taeju-01 <taeju1476@gmail.com>
2025-12-11 20:58:26 +0900

    Title: 정수 사이의 배수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251211/정수 사이의 배수/multiplication-between-numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    int sum_5=0;
+    int sum_7=0;
+    int cnt_5=0;
+    int cnt_7=0;
+    for(int i=a;i<=b;i++){
+        if(i%5==0){
+            cnt_5++;
+            sum_5+=i;
+        }
+        if(i%7==0){
+            cnt_7++;
+            sum_7+=i;
+        }
+
+    }
+    cout<<fixed;
+    cout.precision(1);
+    cout<<sum_5+sum+7<<" "<<double(sum+5+sum+7)/(cnt_5+cnt_7);
+    return 0;
+}
