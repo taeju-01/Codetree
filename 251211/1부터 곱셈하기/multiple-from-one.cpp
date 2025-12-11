9d1e86a5fc2c33cc593ba4bcb10bca23bbca84c7
taeju-01 <taeju1476@gmail.com>
2025-12-11 22:03:51 +0900

    Title: 1부터 곱셈하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251211/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    int prod=1;
+    for(int i=1;i<=10;i++){
+        prod*=i;
+        if(prod>n){
+            cout<<i;
+            break;
+        }
+
+    }
+    return 0;
+}
