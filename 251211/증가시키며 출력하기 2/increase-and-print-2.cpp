cf4538011adc9588f20d751be17e39cf1c60adb7
taeju-01 <taeju1476@gmail.com>
2025-12-11 15:58:06 +0900

    Title: 증가시키며 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i=10;
+    while(i<27){
+        cout<<i<<" ";
+        i++;
+    }
+    return 0;
+}
