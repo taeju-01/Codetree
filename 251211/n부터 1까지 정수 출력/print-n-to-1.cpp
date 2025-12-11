ae8f917203919bb3645679baa56b2dd70381abdc
taeju-01 <taeju1476@gmail.com>
2025-12-11 16:14:57 +0900

    Title: n부터 1까지 정수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    while(n>=1){
+        cout<<n<<" ";
+        n--;
+    }
+    return 0;
+}
