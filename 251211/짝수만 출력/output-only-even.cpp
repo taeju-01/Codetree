aef08ffbd5a36fd36f0a75fabd61d6a320446fb6
taeju-01 <taeju1476@gmail.com>
2025-12-11 16:11:28 +0900

    Title: 짝수만 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    while(a<=b){
+        if(a%2==0){
+            cout<<a<<" ";
+
+        }
+        a++;
+    }
+    return 0;
+}
