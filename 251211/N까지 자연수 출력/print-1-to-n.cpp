57a432b98f6d3e1e8c699bbaf808605200ad9643
taeju-01 <taeju1476@gmail.com>
2025-12-11 16:04:05 +0900

    Title: N까지 자연수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        cout<<i<<" ";
+        i++;
+    }
+    return 0;
+}
