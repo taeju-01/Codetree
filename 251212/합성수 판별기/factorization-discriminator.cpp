e16bc741520dc81d4961bc435bac971f120400ed
taeju-01 <taeju1476@gmail.com>
2025-12-12 16:53:21 +0900

    Title: 합성수 판별기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251212/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    bool satisfied =false;
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0){
+            satisfied =true;
+            break;
+        }
+        else{
+            satisfied =false;
+        }
+        
+    }
+    if(satisfied==true){
+        cout<<"C";
+    }
+    else{
+        cout<<"N";
+    }
+    
+    return 0;
+}
