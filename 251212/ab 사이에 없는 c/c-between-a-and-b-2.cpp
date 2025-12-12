096891229ee7ef62f276df267613e7c4c58abe1b
taeju-01 <taeju1476@gmail.com>
2025-12-12 17:13:24 +0900

    Title: ab 사이에 없는 c, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251212/ab 사이에 없는 c/c-between-a-and-b-2.cpp
+++ 251212/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -5,16 +5,18 @@ int main() {
     // Please write your code here.
     int a,b,c;
     cin>>a>>b>>c;
-    bool satisfied=true;
+    bool satisfied=false;
     for(int i=a;i<=b;i++){
-        if(c%i!=0){
-            satisfied=false;
+        if(i%c==0){
+            satisfied=true;
         }
     }
-    if(satisfied==false){
-        cout<<"YES";
+    if(satisfied==true){
+        cout<<"NO";
     }
-    else
-    cout<<"NO";
+    else{
+    cout<<"YES";
+    }
+
     return 0;
 }
