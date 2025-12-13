d57da3f400cb21613b536dc7039335a3398d3ced
taeju-01 <taeju1476@gmail.com>
2025-12-13 16:31:55 +0900

    Title: 별표 출력하기 10, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251213/별표 출력하기 10/print-star-10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    for(int i=1;i<=2*n;i++){
+        if(i%2==0){
+            for(int j=i/2;j<=n;j++){
+                cout<<"* ";
+            }
+        }
+        else{
+            for(int j=(4*n)-(3*i);j<2*n;j++){
+                cout<<"* ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
