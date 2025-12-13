2e61cfb1452acda93350e5cf3cc49648d9a3b795
taeju-01 <taeju1476@gmail.com>
2025-12-13 15:52:46 +0900

    Title: 별표 출력하기 8, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251213/별표 출력하기 8/print-star-8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(i%2==0){
+            for(int j=0;j<i;j++){
+                cout<<"* ";
+            }
+        }
+        else{
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
