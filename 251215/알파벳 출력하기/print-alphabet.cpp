dc83ed6f1da8f33bf678d6fa0cbaeb361dbb1fa6
taeju-01 <taeju1476@gmail.com>
2025-12-15 16:25:06 +0900

    Title: 알파벳 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251215/알파벳 출력하기/print-alphabet.cpp
+++ 251215/알파벳 출력하기/print-alphabet.cpp
@@ -9,6 +9,9 @@ int main() {
     for(int i=0;i<n;i++){
         for(int j=0;j<=i;j++){
             cout<<cnt;
+            
+            if(int cnt==90)
+            cnt=64;
             cnt++;
         }
         cout<<endl;
