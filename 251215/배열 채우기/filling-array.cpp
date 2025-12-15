6031146dce11af40d7c76606c5caffcafd452d95
taeju-01 <taeju1476@gmail.com>
2025-12-15 21:56:28 +0900

    Title: 배열 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251215/배열 채우기/filling-array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10];
+    int cnt=0;
+   
+    for(int i=0;i<10;i++){
+        cin>>arr[i];
+        cnt++;
+        if(arr[i]==0){
+        cnt--;
+        break;
+        }
+    }
+    for(int i=cnt-1;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
