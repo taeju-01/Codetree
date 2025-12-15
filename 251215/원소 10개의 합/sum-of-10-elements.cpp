744e4ebae01f95b326a80965f6c42f5c2684d15f
taeju-01 <taeju1476@gmail.com>
2025-12-15 19:58:59 +0900

    Title: 원소 10개의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251215/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int sum=0;
+    for(int i=0;i<10;i++){
+        int a;
+        cin>>a;
+        sum+=a;
+    }
+    cout<<sum;
+    return 0;
+}
