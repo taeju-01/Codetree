2cd7c3b26e0b5d7b5bc2a87fa041ec02b0f25f6f
taeju-01 <taeju1476@gmail.com>
2025-12-15 16:37:59 +0900

    Title: ab곱 N번 구하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251215/ab곱 N번 구하기/a-multiple-b-n-times.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        int total=0;
+        int a,b;
+        cin>>a>>b;
+        for(int j=a;j<=b;j++){
+        total+=a;
+        
+        
+    }
+    cout<<total<<endl;
+    }
+    return 0;
+}
