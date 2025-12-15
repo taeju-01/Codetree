5272e34365642e4e5ced39d1bd91455b2287c56e
taeju-01 <taeju1476@gmail.com>
2025-12-15 16:40:49 +0900

    Title: ab곱 N번 구하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 251215/ab곱 N번 구하기/a-multiple-b-n-times.cpp
+++ 251215/ab곱 N번 구하기/a-multiple-b-n-times.cpp
@@ -7,11 +7,13 @@ int main() {
     cin>>n;
     
     for(int i=0;i<n;i++){
-        int total=0;
+        
         int a,b;
         cin>>a>>b;
+        int total=1;
         for(int j=a;j<=b;j++){
-        total+=a;
+            
+        total*=j;
         
         
     }
