1e0c65faf3394b931897d3d4318c97e0798062d6
taeju-01 <taeju1476@gmail.com>
2025-12-15 17:41:35 +0900

    Title: 완전수, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251215/완전수/perfect-number.cpp
+++ 251215/완전수/perfect-number.cpp
@@ -6,16 +6,18 @@ int st, ed;
 
 int main() {
     cin >> st >> ed;
-    int total=0;
+    
     int cnt=0;
     for(int i=st;i<=ed;i++){
+        int total=0;
         for(int j=1;j<i;j++){
             if(i%j==0){
                 total+=j;
             }
         }
-        if(total==i)
+        if(total==i){
         cnt++;
+        }
         
         
     }
