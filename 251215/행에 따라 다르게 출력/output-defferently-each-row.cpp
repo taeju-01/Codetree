11a9e75ce23c15ed086c4519b3085ecc288af227
taeju-01 <taeju1476@gmail.com>
2025-12-15 15:20:44 +0900

    Title: 행에 따라 다르게 출력, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 251215/행에 따라 다르게 출력/output-defferently-each-row.cpp
+++ 251215/행에 따라 다르게 출력/output-defferently-each-row.cpp
@@ -5,18 +5,20 @@ int main() {
     // Please write your code here.
     int n;
     cin>>n;
-    int cnt=1;
+    int cnt=0;
     for(int i=0;i<n;i++){
         if(i%2==0){
             for(int j=0;j<n;j++){
-                cout<<cnt<<" ";
                 cnt++;
+                cout<<cnt<<" ";
+                
             }
         }
         else{
             for(int j=0;j<n;j++){
-                cout<<cnt<<" ";
                 cnt+=2;
+                cout<<cnt<<" ";
+                
             }
         }
         cout<<endl;
