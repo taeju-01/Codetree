cba736beeb9e6d131a54597e3c24e9acdea2eb69
taeju-01 <taeju1476@gmail.com>
2025-12-15 15:32:18 +0900

    Title: 구구단 만들기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251215/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    for(int i=2;i<=8;i+=2){
+        for(int j=b;j>=a;j--){
+            cout<<j<<" * "<<i<<" = "<<j*i;
+            if(j>a)
+            cout<<" / ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
