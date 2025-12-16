659ab16b5b797b9692e22f76ab41659fb109b74a
taeju-01 <taeju1476@gmail.com>
2025-12-16 21:37:09 +0900

    Title: 나눗셈의 나머지, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251216/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    int s;
+    int count_arr[4]={0, };
+    while(a>0){
+        s=a%b;
+        a=a/b;
+        count_arr[s]++;
+    }
+    int sum=0;
+    for(int i=0;i<4;i++){
+        sum=sum+(count_arr[i]*count_arr[i]);
+
+        
+    }
+    cout<<sum;
+    return 0;
+}
