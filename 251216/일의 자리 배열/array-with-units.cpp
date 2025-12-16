6e454a33d57adf0b61059c901fcb7a5e46972916
taeju-01 <taeju1476@gmail.com>
2025-12-16 20:12:53 +0900

    Title: 일의 자리 배열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251216/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    int arr[10];
+    arr[0]=a;
+    arr[1]=b;
+    for(int i=2;i<10;i++){
+        arr[i]=(arr[i-2]+arr[i-1])%10;
+        
+
+    }
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
