9007904c841d3ea008e7edb55e207d9c29630dce
taeju-01 <taeju1476@gmail.com>
2025-12-16 15:25:16 +0900

    Title: 특정 위치의 문자 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251216/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c[10];
+    for(int i=0;i<10;i++){
+        cin>>c[i];
+    }
+    cout<<c[1]<<" "<<c[4]<<" "<<c[7];
+    return 0;
+}
