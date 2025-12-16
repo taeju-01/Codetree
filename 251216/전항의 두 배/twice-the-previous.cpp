6e8f9787ef7e4b711d451a63b264493ee5032244
taeju-01 <taeju1476@gmail.com>
2025-12-16 20:30:18 +0900

    Title: 전항의 두 배, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251216/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    int arr[10]={a,b};
+    for(int i=2;i<10;i++){
+        arr[i]=arr[i-1]+2*arr[i-2];
+    }
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+        
+    }
+    
+    return 0;
+}
