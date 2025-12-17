c22f32b12cf7b9b0a647ec04b567054d1cddc8a5
taeju-01 <taeju1476@gmail.com>
2025-12-17 15:20:43 +0900

    Title: 배열 놀이, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251217/배열 놀이/play-with-array.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,q;
+    cin>>n>>q;
+    int arr[n];
+    int arr_q[q];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int t,a,b;
+ 
+    int w;
+    int d=0;
+    for(int i=0;i<q;i++){
+        cin>>t;
+        if(t==3){
+            cin>>a>>b;
+        }
+        else{
+            cin>>a;
+        }
+
+        if(t==1){
+            cout<<arr[a-1]<<endl;
+        }
+        else if(t==2){
+               int cnt=0;
+            for(int i=0;i<n;i++){
+                
+                if(arr[i]==a){
+                    d=i;
+                    cnt++;
+                }
+            }
+                if(cnt==1){
+                    cout<<d+1<<endl;
+                }
+                else if(cnt>1){
+                    cout<<arr[0]<<endl;
+            }
+            else{
+                cout<<0<<endl;
+            }
+        }
+        else if(t==3){
+            for(int i=a;i<=b;i++){
+                cout<<arr[i-1]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+    return 0;
+}
