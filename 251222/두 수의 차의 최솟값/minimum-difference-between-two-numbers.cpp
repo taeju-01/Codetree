dd81a1df78e3adb3e5be8405a36cb50e61f20cb3
taeju-01 <taeju1476@gmail.com>
2025-12-22 16:54:25 +0900

    Title: 두 수의 차의 최솟값, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251222/두 수의 차의 최솟값/minimum-difference-between-two-numbers.cpp
+++ 251222/두 수의 차의 최솟값/minimum-difference-between-two-numbers.cpp
@@ -10,7 +10,7 @@ int main() {
         cin>>a[i];
     }
     int min=100;
-    for(int i=n;i>=0;i--){
+    for(int i=n-1;i>=0;i--){
         for(int j=i-1;j>=0;j--){
             int minus=a[i]-a[j];
 
