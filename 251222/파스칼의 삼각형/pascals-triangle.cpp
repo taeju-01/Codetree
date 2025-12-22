79a2dfd1b0eb453e655cb0b95e4ee5bf8e438684
taeju-01 <taeju1476@gmail.com>
2025-12-22 22:28:07 +0900

    Title: 파스칼의 삼각형, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251222/파스칼의 삼각형/pascals-triangle.cpp
+++ 251222/파스칼의 삼각형/pascals-triangle.cpp
@@ -16,7 +16,7 @@ int main() {
         }
     }
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+        for(int j=0;j<i+1;j++){
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
