93f7e88bce4fe487dba20490bf7b40074ffd4365
taeju-01 <taeju1476@gmail.com>
2025-12-22 16:10:25 +0900

    Title: 500 근처의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251222/500 근처의 수/near-500.cpp
+++ 251222/500 근처의 수/near-500.cpp
@@ -4,42 +4,25 @@ using namespace std;
 int main() {
     // Please write your code here.
     int a[10];
+
     for(int i=0;i<10;i++){
         cin>>a[i];
+ 
     }
-    int a_1[10];
-    int a_2[10];
+    int max=1;
+    int min=1000;
     
     for(int i=0;i<10;i++){
-        if(a[i]<500){
-            a_1[i]=a[i];
-            
+        if(a[i]>max&&a[i]<500){
+            max=a[i];
+        }
+        if(min>a[i]&&a[i]>500){
+            min=a[i];
+        }
+    }
 
-        }
-        if(a[i]>500){
-            a_2[i]=a[i];
-        }
-        // cout<<a_1[i]<<" ";
-        
-        
-        cout<<a_2[i]<<" ";
-    }
     
-    int max=a_1[0];
-    int min=a_2[0];
-    for(int i=0;i<10;i++){
-        if(a_1[i]>max){
-            max=a_1[i];
-        }
-
-    }
-    for(int i=0;i<10;i++){
-        if(min>a_2[i]&&a_2[i]){
-            min=a_2[i];
-        }
-    }
-    
-    // cout<<min;
+    cout<<max<<" "<<min;
 
     return 0;
 }
