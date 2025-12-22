1856019dd0ff9279654b42e3b1b94b9e1a9d9e5f
taeju-01 <taeju1476@gmail.com>
2025-12-22 17:43:10 +0900

    Title: 배열의 평균, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251222/배열의 평균/ave-of-array.cpp
+++ 251222/배열의 평균/ave-of-array.cpp
@@ -15,21 +15,32 @@ int main() {
     for(int i=0;i<2;i++){
           double sum_1=0;
         for(int j=0;j<4;j++){
-             sum+=arr[i][j];
+             sum_1+=arr[i][j];
         }
         cout<<fixed;
         cout.precision(1);
-        row[i]=sum / 4;
+        row[i]=sum_1 / 4;
         cout<<row[i]<<" ";
     }
+    cout<<endl;
     for(int j=0;j<4;j++){
         double sum_2=0;
         for(int i=0;i<2;i++){
-            sum+=arr[i][j];
+            sum_2+=arr[i][j];
         }
-        col[j]=sum_1/2;
-        cout<<col[i]<<" ";
+        col[j]=sum_2/2;
+        cout<<col[j]<<" ";
     }
+    cout<<endl;
+    double sum_3=0;
+    for(int i=0;i<2;i++){
+        sum_3+=row[i];
+        
+    }
+    for(int j=0;j<4;j++){
+        sum_3+=col[j];
+    }
+    cout<<sum_3/6;
     
     return 0;
 }
