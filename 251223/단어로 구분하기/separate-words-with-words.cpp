b5af6a75bdb95874e6a57e55796b3d4a895eaa35
taeju-01 <taeju1476@gmail.com>
2025-12-23 16:49:13 +0900

    Title: 단어로 구분하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251223/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    for(int i=0;i<10;i++){
+        cin>>str;
+        cout<<str<<endl;
+
+    }
+    return 0;
+}
