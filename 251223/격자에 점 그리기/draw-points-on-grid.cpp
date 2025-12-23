8e7cb45c3f1bd3d3c1c92f986630912c5d6c85b7
taeju-01 <taeju1476@gmail.com>
2025-12-23 15:23:15 +0900

    Title: 격자에 점 그리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251223/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,m;
+    cin>>n>>m;
+    int arr[10][10]={};
+    int cnt=1;
+    for(int i=0;i<m;i++){
+        int r,c;
+        cin>>r>>c;
+        arr[r][c]=cnt;
+        cnt++;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
