d31956425628c11358b560bacd0221568cbe09ad
taeju-01 <taeju1476@gmail.com>
2025-12-24 22:06:23 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 13ms, Memory: 1MB, Status: Passed - Codetree

--- 251224/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.cpp
+++ 251224/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.cpp
@@ -8,22 +8,25 @@ int q;
 int main() {
     cin >> input_str >> q;
     int len=input_str.length();
+    int query_type;
     for (int i = 0; i < q; i++) {
-        int query_type;
         cin >> query_type;
         if(query_type==1){
             input_str=input_str.substr(1,len-1)+input_str.substr(0,1);
             cout<<input_str<<endl;
         }
-        if(query_type==2){
+        else if(query_type==2){
             input_str=input_str.substr(len-1,1)+input_str.substr(0,len-1);
             cout<<input_str<<endl;
         }
-        if(query_type==3){
+        else if(query_type==3){
+            string q3="";
             for(int i=len-1;i>=0;i--){
-                cout<<input_str[i];
+                q3+=input_str[i];
             }
-            cout<<endl;
+            input_str=q3;
+            cout<<input_str<<endl;
+            
         }
     }
 
