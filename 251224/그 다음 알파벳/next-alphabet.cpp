b0b6ed2de50c9ddeda2a35e615e8c4ce12762305
taeju-01 <taeju1476@gmail.com>
2025-12-24 22:23:56 +0900

    Title: 그 다음 알파벳, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251224/그 다음 알파벳/next-alphabet.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char a;
+    cin>>a;
+    int n=(int)a+1;
+    if(a=='z'){
+        cout<<'a';
+    }
+    else{
+        cout<<(char)n;
+    }
+    return 0;
+}
