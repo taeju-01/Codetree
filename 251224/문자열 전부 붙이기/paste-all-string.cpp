69fc97fd664ad8e686fb66661b8365f4a1576a12
taeju-01 <taeju1476@gmail.com>
2025-12-24 16:07:24 +0900

    Title: 문자열 전부 붙이기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251224/문자열 전부 붙이기/paste-all-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    string a;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        cout<<a;
+    }
+    return 0;
+}
