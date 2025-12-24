444f322db67dd3fa71b2c8917e2df370f220b79e
taeju-01 <taeju1476@gmail.com>
2025-12-24 21:12:41 +0900

    Title: 문자 제거하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251224/문자 제거하기/del-char.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a;
+    cin>>a;
+    int len=a.length();
+    int n;
+    while(len>1){
+        
+        cin>>n;
+        if(len<n){
+            a.erase(len-1,1);
+            len--;
+            
+        }
+        else{
+            a.erase(n,1);
+            len--;
+            
+        }
+        cout<<a<<endl;
+    }
+    return 0;
+}
