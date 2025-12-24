7e941db00806dc320733f0cec2c186b25287e2fe
taeju-01 <taeju1476@gmail.com>
2025-12-24 17:25:52 +0900

    Title: 문자 교체하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251224/문자 교체하기/changing-char.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a;
+    string b;
+    cin>>a>>b;
+    string c="";
+    for(int i=0;i<=1;i++){
+        b[i]=a[i];
+    }
+    cout<<b;
+    
+    return 0;
+}
