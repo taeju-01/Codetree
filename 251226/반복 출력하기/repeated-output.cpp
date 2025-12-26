d403b706be9169e00aa841106da73be649ff6e67
taeju-01 <taeju1476@gmail.com>
2025-12-26 22:11:20 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251226/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+void Nlines(int N){
+    for(int i=0;i<N;i++){
+        cout<<"12345^&*()_"<<endl;
+    }
+}
+
+int main() {
+    cin >> N;
+    Nlines(N);
+
+    // Please write your code here.
+
+    return 0;
+}
